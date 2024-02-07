29ce6d2c0a5e42656d1356b31beb029eadb387db
bekzod <bekzodshodiyev999@gmail.com>
2024-02-07 09:32:59 -0600

    Commi_1

---
+++ src/main/java/org/example/MyBot.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyBot extends TelegramLongPollingBot{
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("Salom siz Bekzodning Sinov botidasiz");
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "BEKZODSINOVBOT";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6188028528:AAHzO98Xploh3JeJdO1jdpH-4f5_7fCu_6k";
+    }
+}
