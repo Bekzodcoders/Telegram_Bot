f25006f450ed0415cb574625b8dd742365fbeeaa
bekzod <bekzodshodiyev999@gmail.com>
2024-02-07 09:46:57 -0600

    Commit_2

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class MyBot extends TelegramLongPollingBot{
@@ -8,6 +9,9 @@ public class MyBot extends TelegramLongPollingBot{
     @Override
     public void onUpdateReceived(Update update) {
         System.out.println("Salom siz Bekzodning Sinov botidasiz");
+        SendMessage sendMessage = new SendMessage();
+
+
     }
 
     @Override
