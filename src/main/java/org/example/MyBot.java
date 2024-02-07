81ad6a951312abf0b47d9862bfe78a0c5e9e7bf9
Bekzod <122850134+Bekzodcoders@users.noreply.github.com>
2024-02-07 18:20:10 +0500

    Commit_3

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -2,16 +2,32 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class MyBot extends TelegramLongPollingBot{
+public class MyBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Salom siz Bekzodning Sinov botidasiz");
+        try {
+            Message message = update.getMessage();
+            String text = message.getText();
+
+            if ("/start".equals(text)) {
+                String username = message.getFrom().getFirstName();
+                sendMsg(message.getChatId(), "Assalomu alekum, " + username + " Botga xush kelibsiz");
+            }
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendMsg(Long chatId, String text) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
-
-
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+        execute(sendMessage);
     }
 
     @Override
